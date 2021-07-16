64fef091d66b7f4772e6066fc5b9a941633df64e
MAzaladze <mariamazaladze@yahoo.com>
2021-07-17 01:57:59 +0400

    first commit

---
+++ src/test/java/SelenideBasics2Test.java
@@ -0,0 +1,42 @@
+import com.codeborne.selenide.*;
+import com.codeborne.selenide.impl.SelenideElementListIterator;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class SelenideBasics2Test {
+
+
+    @Test
+    public void booksFound() { open("https://demoqa.com/books");
+        ElementsCollection books = $(".rt-tbody").$$(".rt-tr-group");
+        ElementsCollection bookPublisher=books.filterBy( text( "O'Reilly Media" ) );
+        ElementsCollection bookNameJavaScript=bookPublisher .filterBy( text( "JavaScript" ) );
+        List<String> result = bookNameJavaScript.texts();
+        System.out.println(result);
+        }
+    @Test
+    public void booksSize() { open("https://demoqa.com/books");
+       ElementsCollection books = $(".rt-tbody").$$(".rt-tr-group");
+       books.filterBy(Condition.text( "O'Reilly Media" )).filterBy(Condition.text("JavaScript")).shouldHave(CollectionCondition.size(10));
+    }
+    @Test
+    public void booksimg() { open("https://demoqa.com/books");
+        ElementsCollection books = $(".rt-tbody").$$(".rt-tr-group");
+       //mg[src].notempty
+    }
+
+
+
+    }
