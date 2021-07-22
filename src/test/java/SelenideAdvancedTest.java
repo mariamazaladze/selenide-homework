8002597e1a390d5b69197aa3089e0ed7cb37faff
MAzaladze <mariamazaladze@yahoo.com>
2021-07-22 17:22:12 +0400

    2 commit

--- src/test/java/SelenideAdvancedTest.java
+++ src/test/java/SelenideAdvancedTest.java
@@ -1,5 +1,9 @@
-import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.*;
+import com.codeborne.selenide.junit.SoftAsserts;
+import com.codeborne.selenide.testng.ScreenShooter;
+import org.eclipse.jetty.util.component.Container;
 import org.openqa.selenium.Keys;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import java.util.List;
@@ -8,7 +12,18 @@ import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selectors.byTagName;
 import static com.codeborne.selenide.Selenide.*;
 
+
+@Listeners({ ScreenShooter.class})
 public class SelenideAdvancedTest {
+
+    public SelenideAdvancedTest(){
+        Configuration.startMaximized=true;
+        Configuration.savePageSource=false;
+        Configuration.reportsFolder="src/main/resources/Reports";
+        ScreenShooter.captureSuccessfulTests = true;
+        Configuration.screenshots=true;
+    }
+
     @Test
     public void booksFound() {
         SoftAssert softAssert=new SoftAssert();
@@ -19,10 +34,18 @@ public class SelenideAdvancedTest {
         softAssert.assertEquals(result.size(),10);
         System.out.println(books.first().getText());
         String firstbook=books.first().getText();
+
         if (firstbook.contains("Learning JavaScript Design Patterns")){
             System.out.println("good");
         }
+        // (OR)
+        // books.first().shouldHave( Condition.text( "Learning JavaScript Design Patterns" ) );
         books.stream().forEach(el->
                                    {actions().moveToElement(el.$(byClassName("mr-2")).$(byTagName("a"))).keyDown( Keys.CONTROL).click().perform(); });
+
+        softAssert.assertEquals(Configuration.reportsFolder,"src/main/resources/Reports");
+
     }
+
+
 }
